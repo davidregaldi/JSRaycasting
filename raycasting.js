baa39e9438ee11c5aaa45f74af70d3b353cc4862
David Regaldi <david.regaldi@gmail.com>
2026-01-15 20:28:43 +0100

    +add utils: deg2rad func & drawLine func

--- raycasting.js
+++ raycasting.js
@@ -47,3 +47,16 @@ screen.height = data.screen.height;
 screen.style.border = "1px solid black";
 document.body.appendChild(screen);
 const screenContext = screen.getContext("2d");
+
+function degreeToRadians(degree) {
+  const pi = Math.PI;
+  return degree * pi / 180;
+}
+
+function drawLine(x1, y1, x2, y2, cssColor) {
+  screenContext.strokeStyle = cssColor;
+  screenContext.beginPath();
+  screenContext.moveTo(x1, y1);
+  screenContext.lineTo(x2, y2);
+  screenContext.stroke();
+}
