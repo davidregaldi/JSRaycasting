2f57dc99f5dd59edca1b6082bbf63d9e2b1fbe98
David Regaldi <david.regaldi@gmail.com>
2026-01-15 19:54:54 +0100

    data object definition.

---
+++ raycasting.js
@@ -0,0 +1,36 @@
+// Data
+const data = {
+  screen: {
+    width: 640,
+    height: 480,
+    halfWidth: null,
+    halfHeight: null
+  },
+  render: {
+    delay: 30, //milliseconds
+  },
+  rayCasting: {
+    incrementAngle: null,
+    precision: 64
+  },
+  player: {
+    fov: 60,
+    halfFov: null,
+    x: 2,
+    y: 2,
+    angle: 90
+  },
+  map: [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ]
+}
+
